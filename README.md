de4b60a3ba1492c244006ed3d686be0cdaaafe57
Reza <arianpourinfo@gmail.com>
2022-03-17 11:53:25 +0330

    Initialize add-code-of-conduct-1

---
+++ README.md
@@ -0,0 +1,21 @@
+<p align="center"><img src="https://github.com/sammymwangangi/TALL-Dashboard/blob/master/public/images/new-look.jpg" width="800"></p>
+
+## About TALL Dashboard
+
+TALL Dashboard is a dashboard made with:
+
+- Tailwind CSS.
+- AlpineJS.
+- Laravel.
+- Livewire.
+
+TALL Dashboard is still *under development*
+
+## Installation
+- clone the repository
+- composer install/update
+- cp .env.example .env
+- php artisan key:generate
+- npm install
+- npm run dev
+- php artisan serve
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/index', function () {
+   return view('index');
+});
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
+
+//**************Profiles Routes ****************************
+Route::prefix('/profile')->middleware('auth')->group(function(){
+   Route::get('/','ProfileController@index')->name('profile');
+   Route::get('/passwordChange','ProfileController@changePassword');
+   Route::post('/passwordChange/{id}','ProfileController@postChangePassword');
+   Route::get('/editProfile/{id}','ProfileController@editprofile');
+  Route::post('/editprofile/{id}','ProfileController@posteditprofile');
+  // Route::post('/editprofile/{id}', 'ProfileController@cover');
+});;
